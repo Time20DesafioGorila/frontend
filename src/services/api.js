c0c56b0eac4f17039c20849fb06f1dd87403e114
Gabriel F. Vilar <cogumm@gmail.com>
2020-05-04 20:20:42 -0300

    Acessando o endereço da API corretamente.

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const axios = require('axios')
+const axios = require("axios");
 
-const api = axios.create({ baseURL: 'http://localhost:3001' })
+const api = axios.create({ baseURL: "http://3.17.145.171:8080" });
 
-export default api
+export default api;
